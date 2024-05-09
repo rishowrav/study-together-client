1c4b3b65c1b131636c1091c62925cb848d8ff1ec
rishowrav <rishowrav@gmail.com>
2024-05-10 00:19:05 +0600

    footer section has been created

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,17 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../components/ShareSection/Navbar";
+import Footer from "../components/ShareSection/Footer";
+
+const Main = () => {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <div className="h-[calc(100vh-68px-239px)]">
+        <Outlet></Outlet>
+      </div>
+      <Footer></Footer>
+    </div>
+  );
+};
+
+export default Main;
--- src/Rotues/Route.jsx
+++ src/Rotues/Route.jsx
@@ -1,9 +1,18 @@
 import { createBrowserRouter } from "react-router-dom";
 
+import Main from "../Layout/Main";
+import Home from "../pages/Home/Home";
+
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Main />,
+    children: [
+      {
+        index: true,
+        element: <Home></Home>,
+      },
+    ],
   },
 ]);
 
