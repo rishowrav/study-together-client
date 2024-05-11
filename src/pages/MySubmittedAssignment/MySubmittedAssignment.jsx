e90b0894cd0738569b65b3078aa0f037b26e2fdc
rishowrav <rishowrav@gmail.com>
2024-05-12 00:56:20 +0600

    pending assignment page design has been created

--- src/pages/MySubmittedAssignment/MySubmittedAssignment.jsx
+++ src/pages/MySubmittedAssignment/MySubmittedAssignment.jsx
@@ -1,111 +1,89 @@
+import { Helmet } from "react-helmet";
+
 const MySubmittedAssignment = () => {
   return (
-    <section className="container px-4 mx-auto pt-12">
-      <div className="flex items-center gap-x-3">
-        <h2 className="text-lg font-medium text-gray-800 ">
-          My Submitted Assignments
-        </h2>
+    <section>
+      <Helmet>
+        <title>STUDY TOGETHER | My Submitted Assignment</title>
+      </Helmet>
+      <div className="container px-4 mx-auto pt-12 space-y-6">
+        <div className="flex items-center gap-x-3 ">
+          <h2 className="text-lg font-medium text-gray-800 ">
+            My Submitted Assignments
+          </h2>
 
-        <span className="px-3 py-1 text-xs text-blue-600 bg-blue-100 rounded-full ">
-          05 Assignments
-        </span>
-      </div>
+          <span className="px-3 py-1 text-xs text-blue-600 bg-blue-100 rounded-full ">
+            03 Assignments
+          </span>
+        </div>
 
-      <div className="flex flex-col mt-6">
-        <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-          <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
-            <div className="overflow-hidden border border-gray-200  md:rounded-lg">
-              <table className="min-w-full divide-y divide-gray-200">
-                <thead className="bg-gray-50">
-                  <tr>
-                    <th
-                      scope="col"
-                      className="py-3.5 px-4 text-sm font-normal text-left rtl:text-right text-gray-500"
-                    >
-                      <div className="flex items-center gap-x-3">
-                        <span>Title</span>
-                      </div>
-                    </th>
-
-                    <th
-                      scope="col"
-                      className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500"
-                    >
-                      <span>Status</span>
-                    </th>
-
-                    <th
-                      scope="col"
-                      className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500"
-                    >
-                      <button className="flex items-center gap-x-2">
-                        <span>Marks</span>
-                      </button>
-                    </th>
-
-                    <th
-                      scope="col"
-                      className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500"
-                    >
-                      Obtain Marks
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500"
-                    >
-                      Feedback
-                    </th>
-
-                    <th className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500">
-                      Edit
-                    </th>
-                  </tr>
-                </thead>
-                <tbody className="bg-white divide-y divide-gray-200 ">
-                  <tr>
-                    <td className="px-4 py-4 text-sm text-gray-500  whitespace-nowrap">
-                      This is my first assignment
-                    </td>
-
-                    <td className="px-4 py-4 text-sm text-gray-500  whitespace-nowrap">
-                      jani na ki thakbe
-                    </td>
-
-                    <td className="px-4 py-4 text-sm text-gray-500  whitespace-nowrap">
-                      100
-                    </td>
-                    <td className="px-4 py-4 text-sm whitespace-nowrap">98</td>
-                    <td
-                      title=""
-                      className="px-4 py-4 text-sm text-gray-500  whitespace-nowrap"
-                    >
-                      Lorem ipsum, dolor si adipisicing elit. Ex, provident?..
-                    </td>
-                    <td className="px-4 py-4 text-sm whitespace-nowrap">
-                      <div className="flex items-center gap-x-6">
-                        <button className="text-gray-500 transition-colors duration-200   hover:text-red-500 focus:outline-none">
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            strokeWidth="1.5"
-                            stroke="currentColor"
-                            className="w-5 h-5"
-                          >
-                            <path
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                              d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-                            />
-                          </svg>
-                        </button>
-                      </div>
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-          </div>
+        <div className="overflow-x-auto">
+          <table className="At nulla temporibus table At nulla temporibus table-zebra border border-base-200">
+            {/* head */}
+            <thead className="text-[15px]">
+              <tr>
+                <th></th>
+                <th>Title</th>
+                <th>Status </th>
+                <th>Marks</th>
+                <th>Obtain Marks</th>
+                <th>Feedback</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* row 1 */}
+              <tr>
+                <th>1</th>
+                <td>This is my first assignment</td>
+                <td>
+                  <button className="btn btn-sm btn-warning">Pending</button>
+                </td>
+                <td>77</td>
+                <td>77</td>
+                <td>Lorem ipsum dolor sit amet consectetur adipisicing...</td>
+                <td>
+                  <button className="btn btn-sm btn-error text-white">
+                    Delete
+                  </button>
+                </td>
+              </tr>
+              {/* row 2 */}
+              <tr>
+                <th>2</th>
+                <td>This is my first assignment</td>
+                <td>
+                  {" "}
+                  <button className="btn btn-sm btn-warning">Pending</button>
+                </td>
+                <td>33</td>
+                <td>33</td>
+                <td>Lorem ipsum dolor sit amet consectetur adipisicing...</td>
+                <td>
+                  <button className="btn btn-sm btn-error text-white">
+                    Delete
+                  </button>
+                </td>
+              </tr>
+              {/* row 3 */}
+              <tr>
+                <th>3</th>
+                <td>This is my first assignment</td>
+                <td>
+                  {" "}
+                  <button className="btn btn-sm btn-warning">Pending</button>
+                </td>
+                <td>44</td>
+                <td>55</td>
+                <td>Lorem ipsum dolor sit amet consectetur adipisicing...</td>
+                <td>
+                  <button className="btn btn-sm btn-error text-white">
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       </div>
     </section>
--- src/pages/PendingAssignment/PendingAssignment.jsx
+++ src/pages/PendingAssignment/PendingAssignment.jsx
@@ -6,7 +6,63 @@ const PendingAssignment = () => {
       <Helmet>
         <title>STUDY TOGETHER | Pending Assignmen</title>
       </Helmet>
-      <h1 className="text-5xl">Pending Assignment</h1>
+
+      <section className="container px-4 mx-auto pt-12 space-y-6">
+        <div className="flex items-center gap-x-3">
+          <h2 className="text-lg font-medium  ">My Submitted Assignments</h2>
+
+          <span className="px-3 py-1 text-xs text-blue-600 bg-blue-100 rounded-full ">
+            05 Assignments
+          </span>
+        </div>
+
+        <div className="overflow-x-auto">
+          <table className="At nulla temporibus table At nulla temporibus table-zebra border border-base-200">
+            {/* head */}
+            <thead className="text-[15px]">
+              <tr>
+                <th></th>
+                <th>Title</th>
+                <th>Examinee Name </th>
+                <th>Marks</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* row 1 */}
+              <tr>
+                <th>1</th>
+                <td>Cy Ganderton</td>
+                <td>Quality Control Specialist</td>
+                <td>77</td>
+                <td>
+                  <button className="btn btn-sm">Give Mark</button>
+                </td>
+              </tr>
+              {/* row 2 */}
+              <tr>
+                <th>2</th>
+                <td>Hart Hagerty</td>
+                <td>Desktop Support Technician</td>
+                <td>33</td>
+                <td>
+                  <button className="btn btn-sm">Give Mark</button>
+                </td>
+              </tr>
+              {/* row 3 */}
+              <tr>
+                <th>3</th>
+                <td>Brice Swyre</td>
+                <td>Tax Accountant</td>
+                <td>44</td>
+                <td>
+                  <button className="btn btn-sm">Give Mark</button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </section>
     </div>
   );
 };
